d4448d0c68b5b5494003776cf2835ded5987aa58
AB&Cie <56975700+AdrienBARRAL-bot@users.noreply.github.com>
2024-01-05 11:58:40 +0100

    Create custom_events_script.js

---
+++ overrides/scripts/custom_events_script.js
@@ -0,0 +1,80 @@
+// Liste des événements
+var events = [
+    // Tier I
+    "Jour de Marché",
+    "Menace Armée",
+    "Lieu Interdit",
+    "Jerrycan",
+    "Cimetière Tribal",
+    "Troc",
+    "L'Avion des Autres",
+    "Joyeux Anniversaire",
+    "Taxe Insulaire",
+    "La Voyante un Peu Cinglée",
+    // Tier II
+    "Charité",
+    "Crocodile Affamé",
+    "Forte Houle",
+    "Le Guérisseur",
+    "L'Attaque des Autres",
+    "Moisissure",
+    "Prisonnier",
+    "Danse de la Pluie",
+    "Offrande",
+    "Culture de Patates",
+    // Tier III
+    "Camp Déserté",
+    "Le Volcan Gronde",
+    "À l'Attaque",
+    "Otage de Prestige",
+    "Nouvel Ami",
+    "Gilet Pare-balles",
+    "Changement de Carrière",
+    "Bûcheron",
+    "Pillage",
+    "Réserve Secrète"
+];
+
+// Liste des rôles
+var roles = [
+    "Bouffe-tout",
+    "Capitaine",
+    "Charmeuse-de-serpents",
+    "Charognard",
+    "Commercial",
+    "Costaud",
+    "Enfant",
+    "Garde-du-corps",
+    "Hypnotiseur",
+    "Magicienne",
+    "Mannequin",
+    "Naturiste",
+    "Œil-de-lynx",
+    "Pêcheur-au-gros",
+    "Prévoyante",
+    "Psychorigide",
+    "Sourcier",
+    "Sumo",
+    "Survivaliste",
+    "Voyageur-temporel"
+];
+
+// Fonction pour déclencher un événement
+function triggerEvent() {
+    // Logique pour déclencher un événement
+    // Assurez-vous de mettre en œuvre la logique appropriée pour votre modpack
+}
+
+// Fonction pour définir les rôles
+function setRoles() {
+    // Logique pour définir les rôles une fois (max 1) lors du premier événement
+    // Assurez-vous de mettre en œuvre la logique appropriée pour votre modpack
+}
+
+// Exemple d'utilisation des fonctions
+setRoles();
+
+// Boucle pour déclencher les événements
+for (var i = 0; i < 6; i++) {
+    triggerEvent();
+}
